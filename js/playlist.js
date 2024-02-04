e59448191f047be9b00272c55dceb12c794651e7
ashishrautela123 <143207650+ashishrautela123@users.noreply.github.com>
2024-02-04 11:00:12 +0530

    second

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+
+  {
+  name: "Hass Hass",
+    artist: "Diljit Dosanjh",
+    img: "hass_hass_song",
+    audio: "hass_hass_song"},
+  {
+    name:  "Shape Of You",
+    artist: "Ed Sheeran",
+    img: "Shapeof_you",
+    audio: "Shape_of_you"
+  },
+  {
+    name: "Baby",
+    artist: "Justin Bieber, Ludacris",
+    img: "baby_song",
+    audio: "Baby_song"
+  },
+  {
+    name: "Sugar & Brownies",
+    artist: "Dharia",
+    img: "sugar_brownies",
+    audio: "Sugar_brownies_song"
+  }
+]
